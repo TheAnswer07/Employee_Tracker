d4619989bb61487ee8ca75c0f8dc29df54d00c06
TheAnswer07 <moisedia7@gmail.com>
2022-09-28 10:28:29 -0700

    Successfully tested adding a new department in the Terminal and it shows as expected in MySQL Workbench database

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// const express = require('express');
-
 const mysql = require('mysql2');
-
-// const db = mysql.createConnection('mysql://root:rootroot@localhost:3306/employee_tracker_db');
 
 const inquirer = require('inquirer');
 
@@ -24,27 +20,52 @@ const db = mysql.createConnection(
     console.log('MySql successfully connected!')
   });
 
-//   const app = express();
 
 inquirer.prompt([{
     message: 'What would you like to do?',
     type: 'list',
     choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role'],
     name: 'start'
-}]).then(start => {
+}])
+.then(start => {
     console.log(start);
+    console.log(start.start);
+    switch(start.start) {
+        case 'View All Departments':
+            viewAllDepartments();
+        break
+        case 'View All Roles':
+            viewAllRoles();
+        break
+        case 'View All Employees':
+            viewAllEmployees();
+        break
+        case 'Add A Department':
+            addADepartment();
+        break
+        case 'Add A Role':
+            addARole();
+        break
+        case 'Add An Employee':
+            addAEmployee();
+        break
+        case 'Update An Employee Role':
+            updateAnEmployeeRole();
+        
+    }
 })
 
-// app.get('/employee_tracker_db', (req, res) => {
-//     let sql = 'CREATE DATABASE employee_tracker_db'
-//     db.query(sql, (err) => {
-//         if(err) {
-//             throw err
-//         }
-//         res.send('Database created')
-//     });
-// });
+const addADepartment = () => {
+    inquirer.prompt([{
+        message: 'What Department would you like to add?',
+        type: 'input',
+        name: 'name'
+    }])
+    .then(department => {
+        db.query('INSERT INTO departments SET ?', department, err=> {
+            if(err) {console.log(err)};
+        })
+        console.log('A department has been added!');
+    })
+}
 
-// app.listen('3000', () => {
-//     console.log('Server started on port 3000');
-// })
